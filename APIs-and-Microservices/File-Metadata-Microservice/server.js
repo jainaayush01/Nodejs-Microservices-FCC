e7eb5ba99fdf5a7f22efbdf68cde0c50e998dbba
Aayush Jain <imjainaayush@gmail.com>
2021-04-29 22:47:57 +0530

    completed file metadata MS

--- APIs-and-Microservices/File-Metadata-Microservice/server.js
+++ APIs-and-Microservices/File-Metadata-Microservice/server.js
@@ -1,20 +1,30 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+var bodyParser = require('body-parser');
+const upload = require('multer')();
 var app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if(req.file != undefined) {
+    const { originalname: name, mimetype: type, size } = req.file;
+    res.json({name, type, size});
+  }
+  else {
+    res.end('invalid file');
+  }
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
