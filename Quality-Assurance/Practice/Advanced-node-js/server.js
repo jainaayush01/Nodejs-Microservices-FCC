0ce5eee708f59a0828558bacce1abfac038b859a
Aayush Jain <imjainaayush@gmail.com>
2021-05-03 23:41:04 +0530

    complete

--- Quality-Assurance/Practice/Advanced-node-js/server.js
+++ Quality-Assurance/Practice/Advanced-node-js/server.js
@@ -57,19 +57,19 @@ myDB(async (client) => {
   io.on('connection', (socket) => {
     ++currentUsers;
     io.emit('user', {
-      name: socket.request.user.name,
+      name: socket.request.user.username,
       currentUsers,
       connected: true
     });
     socket.on('chat message', (message) => {
-      io.emit('chat message', { name: socket.request.user.name, message });
+      io.emit('chat message', { name: socket.request.user.username, message });
     });
-    console.log('A user has connected');
+    console.log('A user has connected ' + currentUsers + 'online');
     socket.on('disconnect', () => {
-      console.log('A user has disconnected');
       --currentUsers;
+      console.log(`A user has disconnected. ${currentUsers} online`);
       io.emit('user', {
-        name: socket.request.user.name,
+        name: socket.request.user.username,
         currentUsers,
         connected: false
       });
