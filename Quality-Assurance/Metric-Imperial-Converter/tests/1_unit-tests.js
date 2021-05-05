fcd8b26336708816296e513a042423a7538d60b9
Aayush Jain <imjainaayush@gmail.com>
2021-05-05 22:20:07 +0530

    getNum input test done

--- Quality-Assurance/Metric-Imperial-Converter/tests/1_unit-tests.js
+++ Quality-Assurance/Metric-Imperial-Converter/tests/1_unit-tests.js
@@ -5,5 +5,41 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
+  suite('convertHandler.getNum(input)', function() {
+    test('Whole Number Input', function(done) {
+      var input = "1km";
+      assert.equal(convertHandler.getNum(input), 1);
+      done();
+    })
+    
+    test('Decimal Number Input', function(done) {
+      var input = "1.2km";
+      assert.equal(convertHandler.getNum(input), 1.2);
+      done();
+    })
 
+    test('Fractional Input', function(done) {
+      var input = "1/2km";
+      assert.equal(convertHandler.getNum(input), 0.5);
+      done();
+    })
+
+    test('Fractional Input with a Decimal Number', function(done) {
+      var input = "5/2.5km";
+      assert.equal(convertHandler.getNum(input), 2);
+      done();
+    })
+
+    test('Error on a Double Fraction', function(done) {
+      var input = "3/2/1km";
+      assert.equal(convertHandler.getNum(input), 'invalid number');
+      done();
+    })
+
+    test('Default Numerical Input to 1', function(done) {
+      var input = "km";
+      assert.equal(convertHandler.getNum(input), 1);
+      done();
+    })
+  })
 });
