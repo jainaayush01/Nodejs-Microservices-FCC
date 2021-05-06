fb4f307f0ad450eed9d7de9b069771073bbb4f07
Aayush Jain <imjainaayush@gmail.com>
2021-05-06 17:43:28 +0530

    Start IssueTracker Project with boilerplate

---
+++ Quality-Assurance/boilerplate-project-issuetracker/routes/api.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = function (app) {
+
+  app.route('/api/issues/:project')
+  
+    .get(function (req, res){
+      let project = req.params.project;
+      
+    })
+    
+    .post(function (req, res){
+      let project = req.params.project;
+      
+    })
+    
+    .put(function (req, res){
+      let project = req.params.project;
+      
+    })
+    
+    .delete(function (req, res){
+      let project = req.params.project;
+      
+    });
+    
+};
