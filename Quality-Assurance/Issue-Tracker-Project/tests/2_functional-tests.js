ee56d2d547c0e56feda9719e512c903c23c175b1
Aayush Jain <imjainaayush@gmail.com>
2021-05-08 17:00:32 +0530

    get and post tests

--- Quality-Assurance/Issue-Tracker-Project/tests/2_functional-tests.js
+++ Quality-Assurance/Issue-Tracker-Project/tests/2_functional-tests.js
@@ -6,5 +6,149 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-  
+  let _idtest;
+  suite('Tests for POST request to /api/issues/{project}', () => {
+    test('Create an issue with every field', (done) => {
+      chai
+        .request(server)
+        .post('/api/issues/test')
+        .send({
+          issue_title: 'test title',
+          issue_text: 'test text',
+          created_by: 'test author',
+          status_text: 'test status',
+          assigned_to: 'test assignee'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          
+          assert.property(res.body, 'assigned_to');
+          assert.property(res.body, 'status_text');
+          assert.property(res.body, 'issue_text');
+          assert.property(res.body, 'issue_title');
+          assert.property(res.body, 'created_by');
+          assert.property(res.body, 'created_on');
+          assert.property(res.body, 'updated_on');
+          assert.property(res.body, '_id');
+          assert.property(res.body, 'open');
+
+          _idtest = res.body._id;
+          assert.equal(res.body.assigned_to, 'test assignee');
+          assert.equal(res.body.status_text, 'test status');
+          assert.equal(res.body.issue_text, 'test text');
+          assert.equal(res.body.issue_title, 'test title');
+          assert.equal(res.body.created_by, 'test author');
+          assert.equal(res.body.open, true);
+
+          done();
+        })
+    })
+
+    test('Create an issue with only required fields', (done) => {
+      chai
+        .request(server)
+        .post('/api/issues/test')
+        .send({
+          issue_title: 'test title',
+          issue_text: 'test text',
+          created_by: 'test author'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          
+          assert.property(res.body, 'issue_title');
+          assert.property(res.body, 'issue_text');
+          assert.property(res.body, 'created_by');
+          assert.property(res.body, 'assigned_to');
+          assert.property(res.body, 'status_text');
+          assert.property(res.body, 'created_on');
+          assert.property(res.body, 'updated_on');
+          assert.property(res.body, 'open');
+          assert.property(res.body, '_id');
+
+          assert.equal(res.body.issue_title, 'test title');
+          assert.equal(res.body.issue_text, 'test text');
+          assert.equal(res.body.created_by, 'test author');
+          assert.equal(res.body.assigned_to, '');
+          assert.equal(res.body.status_text, '');
+          assert.equal(res.body.open, true);
+
+          done();
+        })
+    })
+
+    test('Create an issue with missing required fields', (done) => {
+      chai
+        .request(server)
+        .post('/api/issues/test')
+        .send({
+          issue_title: 'test title',
+          created_by: 'test author'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          
+          assert.property(res.body, 'error');
+          assert.equal(res.body.error, 'required field(s) missing');
+          
+          done();
+        })
+    })
+  })
+
+
+  suite('Tests for GET request to /api/issues/{project}', () => {
+    test('View issues on a project', (done) => {
+      chai
+        .request(server)
+        .get('/api/issues/test')
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.isArray(res.body)
+          
+          assert.property(res.body[0], 'assigned_to');
+          assert.property(res.body[0], 'status_text');
+          assert.property(res.body[0], 'issue_text');
+          assert.property(res.body[0], 'issue_title');
+          assert.property(res.body[0], 'created_by');
+          assert.property(res.body[0], 'created_on');
+          assert.property(res.body[0], 'updated_on');
+          assert.property(res.body[0], '_id');
+          assert.property(res.body[0], 'open');
+
+          done();
+        })
+    })
+
+    test('View issues on a project with one filter', (done) => {
+      chai
+        .request(server)
+        .get('/api/issues/test')
+        .query({
+          "issue_title": "test title" 
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.isArray(res.body)
+          
+          assert.property(res.body[0], 'assigned_to');
+          assert.property(res.body[0], 'status_text');
+          assert.property(res.body[0], 'issue_text');
+          assert.property(res.body[0], 'issue_title');
+          assert.property(res.body[0], 'created_by');
+          assert.property(res.body[0], 'created_on');
+          assert.property(res.body[0], 'updated_on');
+          assert.property(res.body[0], '_id');
+          assert.property(res.body[0], 'open');
+
+          assert.equal(res.body[0].issue_title, 'test title');
+          done();
+        })
+    })
+  })
 });
