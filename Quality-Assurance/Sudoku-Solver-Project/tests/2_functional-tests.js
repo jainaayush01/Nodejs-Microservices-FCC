95c36940226b859eca71055b91393b3a97ecae7c
Aayush Jain <imjainaayush@gmail.com>
2021-05-10 20:58:25 +0530

    boilerplate sudoku

---
+++ Quality-Assurance/Sudoku-Solver-Project/tests/2_functional-tests.js
@@ -0,0 +1,11 @@
+const chai = require("chai");
+const chaiHttp = require('chai-http');
+const assert = chai.assert;
+const server = require('../server');
+
+chai.use(chaiHttp);
+
+suite('Functional Tests', () => {
+
+});
+
