8d09d2cca866eb7ec77a1dc9b8b677bb3b888abe
Aayush Jain <imjainaayush@gmail.com>
2021-05-12 22:06:42 +0530

    validate func

--- Quality-Assurance/Sudoku-Solver-Project/controllers/sudoku-solver.js
+++ Quality-Assurance/Sudoku-Solver-Project/controllers/sudoku-solver.js
@@ -1,6 +1,13 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    if(!(/^[0-9,.]*$/.test(puzzleString))) {
+      return {error: 'Invalid characters in puzzle'}
+    }
+    if(puzzleString.length !== 81) {
+      return { error: 'Expected puzzle to be 81 characters long' }
+    }
+    return {}
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
--- Quality-Assurance/Sudoku-Solver-Project/routes/api.js
+++ Quality-Assurance/Sudoku-Solver-Project/routes/api.js
@@ -2,17 +2,34 @@
 
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
-module.exports = function (app) {
-  
+module.exports = function(app) {
+
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.post('/api/check', (req, res) => {
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+  });
 
-    });
+  app.post('/api/solve', (req, res) => {
+    console.log(req.body);
+    const puzzle = req.body.puzzle;
+    if (!puzzle) {
+      res.json({ error: 'Required field missing' });
+    }
+    else {
+      let validate = solver.validate(puzzle);
+      if (validate.hasOwnProperty("error")) {
+        res.json({ error: validate.error });
+      }
+      else {
+        let solve = solver.solve(puzzle);
+        if (solve.hasOwnProperty("error")) {
+          res.json({ error: solve.error });
+        }
+        else {
+          res.json({ solution: solve.solution });
+        }
+      }
+    }
+  });
 };
